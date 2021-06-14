107a658d6c5e7b295d4c0ea789824f4e43c7f31c
dell <dell@DESKTOP-HUGSVRV>
2021-06-14 15:57:06 +0530

    first commit

---
+++ DemoProject/src/main/java/com/coforge/training/Employee.java
@@ -0,0 +1,8 @@
+package com.coforge.training;
+
+public class Employee {
+	public static void main(String[] args) {
+		System.out.println("Way to go jenkins!");
+	}
+
+}
---
+++ TrialProject/src/main/java/com/coforge/training/Greeting.java
@@ -0,0 +1,10 @@
+package com.coforge.training;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome jenkins!");
+
+	}
+
+}
